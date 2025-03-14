b18b0ab1a7bc6b661e8b950dbe861b5bcb95e6ce
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-14 13:30:30 +0900

    __main__.pyx

--- prjbuilder/__main__.pyx
+++ prjbuilder/__main__.pyx
@@ -1,1 +1,5 @@
-pass
+DEF __MYDIR__ __file__[:-11]
+DEF __PTOML__ "pyproject.toml"
+DEF __BUILD__ "python -m build"
+DEF __PYZIN__ 'python -m zipapp pyx -m "__init__:main"
+DEF __WORKS__ ["..", "mere", "merec", "__1nit__", "__pkg__", "pyx"]
