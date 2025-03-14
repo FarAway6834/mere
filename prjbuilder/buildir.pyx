cade0f36b1020d0a03cad10224d95bea5bdc33c3
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-14 13:27:53 +0900

    buildir.pyx

--- prjbuilder/buildir.pyx
+++ prjbuilder/buildir.pyx
@@ -2,4 +2,4 @@ DEF __MYDIR__ __file__[:-11]
 DEF __PTOML__ "pyproject.toml"
 DEF __BUILD__ "python -m build"
 DEF __PYZIN__ 'python -m zipapp pyx -m "__init__:main"
-DEF __WORKS__ ["..", "mere", "__1nit__", "__pkg__", "pyx"]
+DEF __WORKS__ ["..", "mere", "merec", "__1nit__", "__pkg__", "pyx"]
